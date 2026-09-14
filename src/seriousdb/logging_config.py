8fa223c66b9e00a3b3de0c2716be2eb1d5b323dd
aditya-sutar-45 <aditya1.sutar@outlook.com>
2026-09-15 01:33:03 +0530

    removed print from logging config

--- src/seriousdb/logging_config.py
+++ src/seriousdb/logging_config.py
@@ -1,19 +1,30 @@
 import logging
 import os
+import sys
+
+logger = logging.getLogger(__name__)
 
 
 def configure_logging() -> None:
     log_level_string = os.getenv("LOG_LEVEL", "INFO").upper()
 
     levels = logging.getLevelNamesMapping()
+    invalid_level = False
+
     if log_level_string in levels:
         log_level = levels[log_level_string]
     else:
-        print(
-            f"WARNING '{log_level_string}' is not a valid log level. Falling back to 'INFO'."
-        )
+        invalid_level = True
         log_level = logging.INFO
 
     logging.basicConfig(
-        level=log_level, format="[%(asctime)s] %(levelname)s [%(name)s] %(message)s"
+        level=log_level,
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        stream=sys.stdout,
     )
+
+    if invalid_level:
+        logger.warning(
+            "'%s' is not a valid log level. Falling back to 'INFO'.",
+            log_level_string,
+        )
